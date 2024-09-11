6b9fe7c1d1182e10b21c6d2d688e970d6bfe7d70
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-10 21:30:41 -0400

    docs: add annotations to archives. In time: `App.vue` doesnt have an layout, there are just default configs. The real global layout is `default.vue`

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
 // Plugins
 import { registerPlugins } from '@/plugins'
 
-// Components
+// Importa o app
 import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
 
+// Criação do app
 const app = createApp(App)
 
+// Registro de plugins (rotas, o proprio app, middlewares...)
 registerPlugins(app)
 
+// Montar componentes e renderizar
 app.mount('#app')
