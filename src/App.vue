f72b4aca63d6a9bdd297716516088b56b527f63a
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-10 21:41:07 -0400

    feat: add global layout config

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawerToggle = !drawerToggle"></v-app-bar-nav-icon>
-      <v-app-bar-title>Eventfy - Gerencie seus eventos</v-app-bar-title>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawerToggle">
-      <v-list>
-        <v-list-item to="/">
-          <v-list-item-title>Início</v-list-item-title>
-        </v-list-item>
-        <v-list-item to="/newEvent">
-          <v-list-item-title>Novo evento</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-main>
       <!-- Onde vai renderizar o contéudo das pág. -->
       <router-view />
@@ -22,6 +8,5 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const drawerToggle = ref(false)
+
 </script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <v-app>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawerToggle = !drawerToggle"></v-app-bar-nav-icon>
+      <v-app-bar-title>Eventfy - Gerencie seus eventos</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawerToggle">
+      <v-list>
+        <v-list-item to="/">
+          <v-list-item-title>Início</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/newEvent">
+          <v-list-item-title>Novo evento</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view />
     </v-main>
@@ -9,5 +23,6 @@
 </template>
 
 <script setup>
-  //
+import { ref } from 'vue'
+const drawerToggle = ref(false)
 </script>
