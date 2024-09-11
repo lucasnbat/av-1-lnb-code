72a68cc3fe3763d7dfc4efdfb10286869aa8a38f
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-10 21:18:48 -0400

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawerToggle = !drawerToggle"></v-app-bar-nav-icon>
+      <v-app-bar-title>Eventfy - Gerencie seus eventos</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer>
+
+    </v-navigation-drawer>
+    <v-main>
+      <!-- Onde vai renderizar o contéudo das pág. -->
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const drawerToggle = ref(false)
+</script>
