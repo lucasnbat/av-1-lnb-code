454d9c370baeb55b284d7ea9f086fba6588409c0
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-12 15:27:04 -0400

    feat: add `EventInstancePage.vue` skeleton

---
+++ src/pages/EventInstancePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <h1>Nome do evento</h1>
+    <p>Descrição do evento</p>
+
+    <h2>Convidados</h2>
+    <ul>
+      <li>Convidado 1</li>
+      <li>Convidado 2</li>
+      <li>Convidado 3</li>
+    </ul>
+    <p>Nenhum convidado adicionado.</p>
+  </v-container>
+</template>
+
+<script>
+//
+</script>
